74b796073fa9be300b6496d03d6531fce9a95c15
alexpuzi <alex.puzi@gmail.com>
2021-04-16 09:07:10 +0300

    begin work Alex Puzi

---
+++ src/main/java/ru/exemp/panpartizan/gb/springForGBVers5/SpringForGbVers5Application.java
@@ -0,0 +1,13 @@
+package ru.exemp.panpartizan.gb.springForGBVers5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForGbVers5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForGbVers5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/exemp/panpartizan/gb/springForGBVers5/SpringForGbVers5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exemp.panpartizan.gb.springForGBVers5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForGbVers5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
